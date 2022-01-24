d490a82ea351a2746570adb9701ee8a6b1e7dceb
Nguyễn Đắc Toàn <nguyendactoankma@gmail.com>
2022-01-24 23:02:01 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,43 +1,46 @@
 package main
 
 import (
-	// "go-crawl/database"
+	"go-crawl/database"
 	"go-crawl/feeds"
 	"go-crawl/handle"
-
-	// repoimpl "go-crawl/repository/repo_impl"
+	repoimpl "go-crawl/repository/repo_impl"
+	"sync"
 	"time"
 )
 
 func main() {
-	// mg := &database.Mongo{}
-	// mg.CreateConn()
+	mg := &database.Mongo{}
+	mg.CreateConn()
 
-	// handle := handle.Handle{
-	// 	Repo: repoimpl.NewRepo(mg),
-	// }
+	handle := handle.Handle{
+		Repo: repoimpl.NewRepo(mg),
+	}
 
-	// var wg sync.WaitGroup
-	// wg.Add(2)
+	var wg sync.WaitGroup
+	wg.Add(3)
 
-	// go func() {
-	// 	defer wg.Done()
-	// 	feeds.Masothue(handle.Repo)
-	// }()
+	go func() {
+		defer wg.Done()
+		feeds.Masothue(handle.Repo)
+	}()
 
-	// go func() {
-	// 	defer wg.Done()
-	// 	feeds.JobStreet(handle.Repo)
-	// }()
+	go func() {
+		defer wg.Done()
+		feeds.JobStreet(handle.Repo)
+	}()
 
-	// wg.Wait()
+	go func() {
+		defer wg.Done()
+		feeds.TopCV(handle.Repo)
+	}()
 
-	// feeds.ItViec(handle.Repo)
+	wg.Wait()
 
-	feeds.TopCV()
 	// Schedule crawl
-	// go schedule(24*time.Hour, handle, 1)
-	// schedule(48*time.Hour, handle, 2)
+	go schedule(24*time.Hour, handle, 1)
+	schedule(48*time.Hour, handle, 2)
+	schedule(24*time.Hour, handle, 3)
 }
 
 func schedule(timeSchedule time.Duration, handle handle.Handle, inndex int) {
@@ -51,6 +54,9 @@ func schedule(timeSchedule time.Duration, handle handle.Handle, inndex int) {
 			case 2:
 				<-ticker.C
 				feeds.JobStreet(handle.Repo)
+			case 3:
+				<-ticker.C
+				feeds.TopCV(handle.Repo)
 			}
 		}
 	}()
